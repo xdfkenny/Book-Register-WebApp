ef2b2f394b96e1ef6525547096913ff640534b27
xdfkenny <xdfkenny@gmail.com>
2025-09-23 22:59:16 +0000

    add import { SpeedInsights } from "@vercel/speed-insights/next"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: 'Book Register',
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className="font-body antialiased">
         {children}
         <Toaster />
+        <SpeedInsights />
       </body>
     </html>
   );
